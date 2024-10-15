703c46cedcef8cf5d53efdce512cf5e6e2c03b3b
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-15 17:51:30 +0900

    Support random perturbation

--- conway.cpp
+++ conway.cpp
@@ -1,11 +1,14 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
+#include <pybind11/eigen.h>
+#include <Eigen/Core>
 #include <iostream>
+#include <optional>
 
 namespace py = pybind11;
 
-py::array_t<bool> evolve_conway(py::array_t<bool> input, size_t T, bool perturb) {
+py::array_t<bool> evolve_conway(py::array_t<bool> input, size_t T, std::optional<Eigen::MatrixX2i> perturbs) {
     py::buffer_info buf = input.request();
     if (buf.ndim != 2) {
         throw std::runtime_error("Number of dimensions must be two");
@@ -20,9 +23,26 @@ py::array_t<bool> evolve_conway(py::array_t<bool> input, size_t T, bool perturb)
     bool *input_ptr = (bool *) input.request().ptr;
     bool *output_ptr = (bool *) output.request().ptr;
 
+    py::array_t<bool> tmp({nx, ny});
+    auto tmp_ptr = (bool *) tmp.request().ptr;
+    auto tmp_accessor = tmp.mutable_unchecked<2>();
+
     memcpy(output_ptr, input_ptr, nx * ny * sizeof(bool)); // copy initial state
     auto voxels = output.mutable_unchecked<3>();
     for(size_t t = 0; t < nt - 1; ++t) { // evolve t times
+      if(perturbs) {
+        std::fill(tmp_ptr, tmp_ptr + nx * ny, false);
+        auto delta_x = perturbs->coeff(t, 0);
+        auto delta_y = perturbs->coeff(t, 1);
+        for(size_t i = 0; i < nx; ++i) {
+          for(size_t j = 0; j < ny; ++j) {
+            // TODO: can we do this in place?
+            tmp_accessor(i, j) = voxels(t, (i - delta_x) % nx, (j - delta_y) % ny);
+          }
+        }
+        size_t offset = t * nx * ny;
+        memcpy(output_ptr + offset, tmp_ptr, nx * ny * sizeof(bool));
+      }
       for(size_t i = 0; i < nx; ++i) {
         for(size_t j = 0; j < ny; ++j) {
           size_t nearest_alive_count = 0;
@@ -46,5 +66,5 @@ py::array_t<bool> evolve_conway(py::array_t<bool> input, size_t T, bool perturb)
 }
 
 PYBIND11_MODULE(conway_tower, m) {
-  m.def("evolve_conway", &evolve_conway, "Evolve Conway's game of life");
+  m.def("evolve_conway", &evolve_conway, "Evolve Conway's game of life", py::arg("input"), py::arg("T"), py::arg("perturbs") = std::nullopt);
 }
